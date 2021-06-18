56261c92ae0773c28252e77989da6db01025a8cd
Marat Tazhetdinov <tms1703@gmail.com>
2021-06-18 19:08:16 +0300

    Initial Commit

---
+++ Homework 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework 3
+//
+//  Created by Marat Tazhetdinov on 18.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
